833947ebba5fd7cefa21110195dd14a5e0f716a1
Tomas Igarza <tomasigarza99@gmail.com>
2025-05-02 19:52:49 -0300

    Se agregan stats de seccion

--- airball/src/app/components/stats-display/stats-display.component.ts
+++ airball/src/app/components/stats-display/stats-display.component.ts
@@ -9,11 +9,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './stats-display.component.scss'
 })
 export class StatsDisplayComponent {
+  sectionName: string | null = 'Left Short 2';
+  
   // Mock data
   stats = {
-    totalShots: 56,
-    makes: 32,
-    percentage: 57.1,
+    total: {
+      shots: 56,
+      makes: 32,
+      percentage: 57.1
+    },
+    section: {
+      shots: 10,
+      makes: 5,
+      percentage: 50
+    },
     lastSessionDate: '2025-04-30'
   };
   
